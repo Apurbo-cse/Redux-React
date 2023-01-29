843882c41217e6ab276e03ed91fca5478c247e15
Apurbo Kumar Anup <apurboka@gmail.com>
2023-01-29 12:20:02 +0600

    Index

---
+++ index.js
@@ -0,0 +1,20 @@
+// State
+const initialCounterState = {
+  count: 0,
+};
+const initialUserState = {
+  users: [
+    {
+      name: "Aputbo Roy",
+    },
+  ],
+};
+
+// Action   - Object -type,payload
+
+// Increment Counter
+{
+  type: "INCREMENT";
+}
+
+// Decrement Counter
